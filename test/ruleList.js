944072dfcba900cd58f9674036d1b198025b626c
Mark Birbeck <mark.birbeck@gmail.com>
2015-01-26 15:43:14 +0000

    add test for deep structure but disable for now

--- test/ruleList.js
+++ test/ruleList.js
@@ -30,4 +30,33 @@ describe('rules list', function(){
 
     res.should.equal('(DATE (DAY 01/CD) (MONTH March/NNP) (YEAR 2015/CD)) Chinese/JJ New/NNP Year/NN Dinner/NN');
   });
+
+  it.skip('should match a set of date chunking rules', function(){
+    var tags = 'Mary/NN saw/VBD the/DT cat/NN sit/VB on/IN the/DT mat/NN';
+    var rules = [
+      {
+        ruleType: 'tokens',
+        pattern: '[ { tag:/DT|JJ|NN.*?/ } ]+',
+        result: 'NP'
+      },
+      {
+        ruleType: 'tokens',
+        pattern: '[ { tag:"IN" } ] [ { tag:"NP" } ]',
+        result: 'PP'
+      },
+      {
+        ruleType: 'tokens',
+        pattern: '[ { tag:/VB.*?/ } ] [ { tag:/NP|PP/CLAUSE/ } ]+$',
+        result: 'VP'
+      },
+      {
+        ruleType: 'tokens',
+        pattern: '[ { tag:"NP" } ] [ { tag:"VP" } ]',
+        result: 'CLAUSE'
+      }
+    ];
+    var res = chunker.chunk(tags, rules);
+
+    res.should.equal('(NP Mary/NN) saw/VBD (CLAUSE (NP the/DT cat/NN) (VP sit/VB (PP on/IN (NP the/DT mat/NN))))');
+  });
 });
