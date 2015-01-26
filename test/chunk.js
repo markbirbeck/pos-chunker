a6951fca482d55265b8d3ef5a858f1771c35857d
Mark Birbeck <mark.birbeck@gmail.com>
2015-01-26 06:00:52 +0000

    add support for nested chunks

--- test/chunk.js
+++ test/chunk.js
@@ -21,4 +21,17 @@ describe('chunk', function(){
     var res = chunker.parse(chunks, '[ { chunk:"MONTH" } ] [ { word:"\\d{4}" } ]');
     res.should.equal('8/CD [(MONTH January/NNP) 2014/CD] TO/TO 28/CD [(MONTH March/NNP) 2014/CD]');
   });
+
+  it('should cope with multiple layers of chunking', function(){
+    var tags = '8/CD January/NNP 2014/CD TO/TO 28/CD March/NNP 2014/CD';
+    var months = chunker.convert(tags, '[ { word:/January|February|March|April|May|June|July|August|September|October|November|December/ } ]', 'MONTH');
+
+    months.should.equal('8/CD (MONTH January/NNP) 2014/CD TO/TO 28/CD (MONTH March/NNP) 2014/CD');
+
+    var dates = chunker.convert(months, '[ { word:"\\d{1,2}" } ] [ { chunk:"MONTH" } ] [ { word:"\\d{4}" } ]', 'DATE');
+    dates.should.equal('(DATE 8/CD (MONTH January/NNP) 2014/CD) TO/TO (DATE 28/CD (MONTH March/NNP) 2014/CD)');
+
+    var res = chunker.parse(dates, '[ { chunk:"DATE" } ]');
+    res.should.equal('[(DATE 8/CD (MONTH January/NNP) 2014/CD)] TO/TO [(DATE 28/CD (MONTH March/NNP) 2014/CD)]');
+  });
 });
