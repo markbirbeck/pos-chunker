f4b72a6e07f67ceaaf9bc6c8d700dc5a79998884
Mark Birbeck <mark.birbeck@gmail.com>
2015-01-26 03:04:03 +0000

    add convert function

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var rule = require('./lib/rule');
 
 exports.parse = parse;
+exports.convert = convert;
 
 function parse(tags, re){
   var regexp = new RegExp(rule(re), 'gm');
 
   return tags.replace(regexp, '[$1]');
+}
+
+function convert(tags, re, token){
+  var mapped = parse(tags, re);
+
+  return mapped.replace(/\[(.*?)]/g, '(' + token + ' $1)');
 }
