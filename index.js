c5676e935604192992c454d57e68c859bc273fb3
Mark Birbeck <mark.birbeck@gmail.com>
2015-01-26 07:41:10 +0000

    add processing for a list of chunk rules

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var rule = require('./lib/rule');
 
 exports.parse = parse;
 exports.convert = convert;
+exports.chunk = chunk;
 
 function parse(tags, re){
   var regexp = new RegExp(rule(re), 'gm');
@@ -13,4 +14,15 @@ function convert(tags, re, token){
   var mapped = parse(tags, re);
 
   return mapped.replace(/\[(.*?)]/g, '(' + token + ' $1)');
+}
+
+function chunk(tags, ruleList){
+  var ret = tags;
+
+  ruleList.map(function (rule){
+    if (rule && rule.ruleType === 'tokens'){
+      ret = convert(ret, rule.pattern, rule.result);
+    }
+  });
+  return ret;
 }
--- test/chunk.js
+++ test/chunk.js
@@ -41,4 +41,33 @@ describe('chunk', function(){
     var res = chunker.parse(dates, '[ { chunk:"DATE" } ]');
     res.should.equal('[(DATE 8/CD (MONTH January/NNP) 2014/CD)] TO/TO [(DATE 28/CD (MONTH March/NNP) 2014/CD)]');
   });
+
+  it('should match a set of chunking rules', function(){
+    var tags = '01/CD March/NNP 2015/CD Chinese/JJ New/NNP Year/NN Dinner/NN';
+    var rules = [
+      {
+        ruleType: 'tokens',
+        pattern: '[ { word:/January|February|March|April|May|June|July|August|September|October|November|December/ } ]',
+        result: 'MONTH'
+      },
+      {
+        ruleType: 'tokens',
+        pattern: '[ { word:\\d{1,2} } ](?=\\s[ { chunk:"MONTH" } ])',
+        result: 'DAY'
+      },
+      {
+        ruleType: 'tokens',
+        pattern: '[ { word:\\d{4} } ]',
+        result: 'YEAR'
+      },
+      {
+        ruleType: 'tokens',
+        pattern: '[ { chunk:"DAY" } ] [ { chunk:"MONTH" } ] [ { chunk:"YEAR" } ]',
+        result: 'DATE'
+      }
+    ];
+    var res = chunker.chunk(tags, rules);
+
+    res.should.equal('(DATE (DAY 01/CD) (MONTH March/NNP) (YEAR 2015/CD)) Chinese/JJ New/NNP Year/NN Dinner/NN');
+  });
 });
