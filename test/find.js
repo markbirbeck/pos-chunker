26fca93825c1157b167dce7f50b971ebf539ce2e
Mark Birbeck <mark.birbeck@gmail.com>
2015-02-27 18:20:03 +0000

    remove TODO...since no longer relevant

--- test/find.js
+++ test/find.js
@@ -19,10 +19,6 @@ describe('find tags, words and chunks', function(){
       res.should.equal('They/PRP [refuse/VB] to/TO [permit/VB] us/PRP to/TO [obtain/VB] the/DT refuse/NN permit/NN');
     });
 
-    /**
-     * TODO: Repeating patterns need to take into account that the tokens are space-separated.
-     */
-
     it('should match tokens with a single tag but with a regular expression', function(){
       var tags = 'This/DT is/VBZ some/DT sample/NN text/NN ./. This/DT text/NN can/MD contain/VB multiple/JJ sentences/NNS ./.';
       var res = chunker.chunk(tags, '[ { tag:/DT|NNS?/; } ]+');
