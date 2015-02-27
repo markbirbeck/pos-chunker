608e38b43b7ef58b322752537b4230edf8c9ea52
Mark Birbeck <mark.birbeck@gmail.com>
2015-02-27 01:27:50 +0000

    remove \b on each item since it's not doing anything

--- test/rule.js
+++ test/rule.js
@@ -3,7 +3,7 @@ var rule = require('../lib/rule');
 
 describe('expressions', function(){
   it('should create expression using tag', function(){
-    rule('[ { tag:/VB/; } ]').should.equal('((\\b([^\\s/]*)/(VB)\\b))');
+    rule('[ { tag:/VB/; } ]').should.equal('(([^\\s/]*)/(VB))');
   });
 
   it('should create expression using tag without final semicolon', function(){
@@ -13,22 +13,22 @@ describe('expressions', function(){
   });
 
   it('should create expression using tag with regular expression', function(){
-    rule('[ { tag:/DT|NN.*/; } ]').should.equal('((\\b([^\\s/]*)/(DT|NN.*)\\b))');
+    rule('[ { tag:/DT|NN.*/; } ]').should.equal('(([^\\s/]*)/(DT|NN.*))');
   });
 
   it('should create expression using word', function(){
-    rule('[ { word:/cat/; } ]').should.equal('((\\b(cat)/([A-Z]*)\\b))');
+    rule('[ { word:/cat/; } ]').should.equal('((cat)/([A-Z]*))');
   });
 
   it('should create expression using word with regular expression', function(){
     var res = rule('[ { word:/cat|dog/; } ]');
 
-    res.should.equal('((\\b(cat|dog)/([A-Z]*)\\b))');
+    res.should.equal('((cat|dog)/([A-Z]*))');
   });
 
   it('should create expression using word and tag', function(){
     var res = rule('[ { word:/cat|dog/; tag:"NN" } ]');
 
-    res.should.equal('((\\b(cat|dog)/(NN)\\b))');
+    res.should.equal('((cat|dog)/(NN))');
   });
 });
