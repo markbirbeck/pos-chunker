1b4f0448aef4c23dc52779714669fc65cceaf162
Mark Birbeck <mark.birbeck@gmail.com>
2015-03-03 15:15:55 +0000

    test: add test for lookahead

--- test/convert.js
+++ test/convert.js
@@ -22,4 +22,13 @@ describe('convert', function() {
 
     res.should.equal('(DATE 01/CD March/NNP 2015/CD) Chinese/JJ New/NNP Year/NN Dinner/NN');
   });
+
+  it('should support lookahead', function() {
+    var tags = '01/CD March/NNP 2015/CD Chinese/JJ New/NNP Year/NN Dinner/NN';
+    var res = chunker.chunk(tags, '[ { word:/January|February|March|April|May|June|July|August|September|October|November|December/ } ]', 'MONTH');
+
+    res = chunker.chunk(res, '[ { word:\\d{1,2} } ](?=[ { chunk:MONTH } ])', 'DAY');
+
+    res.should.equal('(DAY 01/CD) (MONTH March/NNP) 2015/CD Chinese/JJ New/NNP Year/NN Dinner/NN');
+  });
 });
