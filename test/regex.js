153746648bf42e7a598606a5524ce9a12c88d260
Mark Birbeck <mark.birbeck@gmail.com>
2015-03-06 17:31:46 +0000

    add a few more basic regex tests

--- test/regex.js
+++ test/regex.js
@@ -8,6 +8,20 @@ require('should');
 var chunker = require('..');
 
 describe('regex rules', function() {
+  it('should match one or more with \'+\'', function() {
+    var tags = '01/CD March/NNP 2015/CD Chinese/JJ New/NNP Year/NN Dinner/NN';
+    var res = chunker.chunk(tags, '[ { tag:NNP? } ]+', 'NOUNS');
+
+    res.should.equal('01/CD [NOUNS March/NNP] 2015/CD Chinese/JJ [NOUNS New/NNP Year/NN Dinner/NN]');
+  });
+
+  it('should match two or more with \'{2,}\'', function() {
+    var tags = '01/CD March/NNP 2015/CD Chinese/JJ New/NNP Year/NN Dinner/NN';
+    var res = chunker.chunk(tags, '[ { tag:NNP? } ]{2,}', 'NOUNS');
+
+    res.should.equal('01/CD March/NNP 2015/CD Chinese/JJ [NOUNS New/NNP Year/NN Dinner/NN]');
+  });
+
   it('should support lookahead', function() {
     var tags = '01/CD March/NNP 2015/CD Chinese/JJ New/NNP Year/NN Dinner/NN';
     var res = chunker.chunk(tags, '[ { word:/January|February|March|April|May|June|July|August|September|October|November|December/ } ]', 'MONTH');
