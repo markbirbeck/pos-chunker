c7205f561ba58ee0835fcedc3d45feba4002b960
Mark Birbeck <mark.birbeck@gmail.com>
2015-03-06 12:05:17 +0000

    fix(#4): spaces between expressions in rules prevent match

--- test/rule.js
+++ test/rule.js
@@ -7,33 +7,27 @@ var rule = require('../lib/rule');
 
 describe('rule', function() {
   it('should create expression using tag', function() {
-    rule('[ { tag:/VB/; } ]').should.equal('(((?:\\s|\\b)([^\\s/]*)/(VB)\\s*))');
-  });
-
-  it('should create expression using tag without final semicolon', function() {
-    var r = 'tag:/VB/';
-
-    rule('[ { ' + r + ' } ]').should.equal(rule('[ { ' + r + '; } ]'));
+    rule('[ { tag:/VB/; } ]').should.equal('(((?:^|\\s|\\b)([^\\s/]+)/(VB)))');
   });
 
   it('should create expression using tag with regular expression', function() {
     rule('[ { tag:/DT|NN.*/; } ]')
-      .should.equal('(((?:\\s|\\b)([^\\s/]*)/(DT|NN.*)\\s*))');
+      .should.equal('(((?:^|\\s|\\b)([^\\s/]*)/(DT|NN.*)))');
   });
 
   it('should create expression using word', function() {
-    rule('[ { word:/cat/; } ]').should.equal('(((?:\\s|\\b)(cat)/([A-Z]*)\\s*))');
+    rule('[ { word:/cat/; } ]').should.equal('(((?:^|\\s|\\b)(cat)/([A-Z]*)))');
   });
 
   it('should create expression using word with regular expression', function() {
     var res = rule('[ { word:/cat|dog/; } ]');
 
-    res.should.equal('(((?:\\s|\\b)(cat|dog)/([A-Z]*)\\s*))');
+    res.should.equal('(((?:^|\\s|\\b)(cat|dog)/([A-Z]*)))');
   });
 
   it('should create expression using word and tag', function() {
     var res = rule('[ { word:/cat|dog/; tag:"NN" } ]');
 
-    res.should.equal('(((?:\\s|\\b)(cat|dog)/(NN)\\s*))');
+    res.should.equal('(((?:^|\\s|\\b)(cat|dog)/(NN)))');
   });
 });
---
+++ test/tidyExpression.js
@@ -0,0 +1,52 @@
+/**
+ * Test that the rule library creates rules correctly.
+ */
+
+require('should');
+var rule = require('../lib/rule');
+
+describe('tidy expressions', function() {
+  describe('spaces', function() {
+    it('should ignore spaces within expression', function() {
+      rule('[ { tag:/VB/ } ]').should.equal(rule('[{ tag:/VB/ }]'));
+      rule('[    { tag:/VB/ }    ]').should.equal(rule('[{ tag:/VB/ }]'));
+      rule('[    { tag:/VB/ }]').should.equal(rule('[{ tag:/VB/ }]'));
+    });
+
+    it('should reduce spaces between expressions', function() {
+      rule('[ { tag:/NN/ } ] [ { tag:/VB/ } ]')
+        .should.equal(rule('[ { tag:/NN/ } ] [ { tag:/VB/ } ]'));
+      rule('[ { tag:/NN/ } ] [ { tag:/VB/ } ]')
+        .should.equal(rule('[ { tag:/NN/ } ]     [ { tag:/VB/ } ]'));
+      rule('[ { tag:/NN/ } ][ { tag:/VB/ } ]')
+        .should.equal(rule('[ { tag:/NN/ } ]     [ { tag:/VB/ } ]'));
+      rule('([ { tag:/NN/ } ])? [ { tag:/VB/ } ]')
+        .should.equal(rule('([ { tag:/NN/ } ])?     [ { tag:/VB/ } ]'));
+    });
+  });
+
+  describe('semicolons between attributes', function() {
+    it('should not matter if there is a final semicolon', function() {
+      rule('[ { tag:/VB/ } ]').should.equal(rule('[ { tag:/VB/; } ]'));
+    });
+  });
+
+  describe('quotes', function() {
+    it('should not matter if there are quotes around expression', function() {
+      rule('[ { tag:"VB" } ]').should.equal(rule('[ { tag:VB } ]'));
+      rule('[ { word:"running" } ]').should.equal(rule('[ { word:running } ]'));
+      rule('[ { word:"running"; tag:VB } ]')
+        .should.equal(rule('[ { word:running; tag:"VB" } ]'));
+    });
+  });
+
+  describe('regex', function() {
+    it('should not matter if there are slashes around expression', function() {
+      rule('[ { tag:/VB.*?/ } ]').should.equal(rule('[ { tag:VB.*? } ]'));
+      rule('[ { word:/run(ning)?/ } ]')
+        .should.equal(rule('[ { word:run(ning)? } ]'));
+      rule('[ { word:/run(ning)?/; tag:/VB.*?/ } ]')
+        .should.equal(rule('[ { word:run(ning)?; tag:VB.*? } ]'));
+    });
+  });
+});
