468d1be3b8275bc4ae48ac107978b19eeb63479e
Mark Birbeck <mark.birbeck@gmail.com>
2015-07-27 09:12:45 +0100

    add failing test

--- test/readme.js
+++ test/readme.js
@@ -20,6 +20,46 @@ describe('README examples', function() {
           );
         });
       });
+
+      describe('chunker.chunk()', function() {
+        it('test', function() {
+          var NP = {
+            ruleType: 'tokens',
+            pattern: '[ { tag:/DT|JJ|NN.*?/ } ]+',
+            result: 'NP'
+          };
+          var PP = {
+            ruleType: 'tokens',
+            pattern: '[ { tag:IN } ] [ { chunk:NP } ]',
+            result: 'PP'
+          };
+          var VP = {
+            ruleType: 'tokens',
+            pattern: '[ { tag:/VB.*?/ } ] [ { chunk:/NP|PP/ } ]+',
+            result: 'VP'
+          };
+          var CLAUSE = {
+            ruleType: 'tokens',
+            pattern: '[ { tag:NP } ] [ { tag:VP } ]',
+            result: 'CLAUSE'
+          };
+          var rules = [NP, PP, VP, CLAUSE];
+
+          chunker.chunk(
+            'The/DT doctor/NN saw/VBD the/DT patient/NN at/IN the/DT surgery/NN ./.',
+            rules
+          ).should.equal(
+            '[CLAUSE [NP The/DT doctor/NN] [VP saw/VBD [NP the/DT patient/NN] [PP at/IN [NP the/DT surgery/NN]]]] ./.'
+          );
+
+          chunker.chunk(
+            'Mary/NN saw/VBD the/DT cat/NN sit/VB on/IN the/DT mat/NN ./.',
+            rules
+          ).should.equal(
+            '[CLAUSE [NP Mary/NN] [VP saw/VBD [NP the/DT cat/NN]]] [VP sit/VB [PP on/IN [NP the/DT mat/NN]]] ./.'
+          );
+        });
+      });
     });
   });
 });
